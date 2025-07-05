3819dec371a02f662e9f55c37c66fdbb8cb46198
Mzoughh <mateo.zough@gmail.com>
2025-07-05 11:29:54 +0200

    endpoint id

--- test_remote_endpoint.py
+++ test_remote_endpoint.py
@@ -8,7 +8,8 @@ from google.cloud import aiplatform
 # --- Vertex AI Configuration ---
 PROJECT_ID = 'gemma-hcls25par-722'  
 REGION = 'europe-west1'            
-ENDPOINT_ID = '8337460333983563776' 
+# ENDPOINT_ID = '8337460333983563776' 
+ENDPOINT_ID = '4563443846247088128'
 
 # --- GCS Bucket Configuration ---
 LOCAL_FILE_PATH = "data/LUMIERE_001_0000.nii.gz"
